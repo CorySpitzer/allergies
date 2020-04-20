a37c9929b7449117227f609649cb66b887462d8e
Cory Spitzer <coryspitzer@gmail.com>
2020-04-20 10:18:26 -0400

    Work for basic examples

--- lib/allergies.rb
+++ lib/allergies.rb
@@ -8,14 +8,32 @@
 # pollen       64
 # cats         128
 
+
 class Score
-    attr_reader :number
+    attr_reader :number, :allergies
 
     def initialize number
         @number = number
+        @allergies = []
+        @dictionary = {1 => 'eggs',
+                       2 => 'peanuts',
+                       4 => 'shellfish',
+                       8 => 'strawberries',
+                       16 => 'tomatoes'}
     end
 
     def names
-        []
+        # Start out with 2^4
+        countdown = 4
+        key = 1
+        while key >= 1
+            key = 2 ** countdown
+            if @number >= key
+                @allergies << @dictionary[key]
+                @number -= key
+            end
+            countdown -= 1
+        end
+        @allergies
     end
 end
--- spec/allergies_spec.rb
+++ spec/allergies_spec.rb
@@ -5,4 +5,8 @@ describe 'Score#allergies' do
     it "is empty for the 0 case" do
         expect(Score.new(0).names).to eq []
     end
+
+    it "is works for a score of 1" do
+        expect(Score.new(1).names).to eq ['eggs']
+    end
 end
