f2074bd2ba07e526aa2b1f54a21362fe3a2e1085
Cory Spitzer <coryspitzer@gmail.com>
2020-04-20 11:08:50 -0400

    Test some more

--- spec/allergies_spec.rb
+++ spec/allergies_spec.rb
@@ -9,4 +9,24 @@ describe 'Score#allergies' do
     it "is works for a score of 1" do
         expect(Score.new(1).names).to eq ['eggs']
     end
+
+    it "is works for a score of 2" do
+        expect(Score.new(2).names).to eq ['peanuts']
+    end
+
+    it "is works for a score of 3" do
+        expect(Score.new(3).names).to eq ['peanuts', 'eggs']
+    end
+
+    it "is works for a score of 4" do
+        expect(Score.new(4).names).to eq ['shellfish']
+    end
+
+    it "is works for a score of 5" do
+        expect(Score.new(5).names).to eq ['shellfish', 'eggs']
+    end
+
+    it "is works for a score of 8" do
+        expect(Score.new(8).names).to eq ['strawberries']
+    end
 end
